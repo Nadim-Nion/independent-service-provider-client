f15c361d8fbba290c56492f4a75e5f98d4ccd0fe
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-01-07 18:32:10 +0600

    about me and checkout content updated

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import image from '../../assets/images/my-image.png';
 
 const AboutMe = () => {
     return (
-        <div>
-            <h2>About Me</h2>
+        <div className='container mx-auto px-32 mt-10'>
+            <div className='w-60 container mx-auto'>
+                <img src={image} alt="" />
+            </div>
+            <div className='mt-4 text-lg'>
+                <h2 className='mb-2'>Hi, This is Nadim Mahmud Nion</h2>
+                <p>My career goal is to become a seasoned Front-end Developer recognized for creating immersive and responsive user interfaces. I aim to deepen my expertise in React, explore advanced state management solutions, and contribute to open-source projects. Additionally, I aspire to stay abreast of emerging technologies, ensuring my skills align with industry trends, while also mentoring and inspiring other developers in the community.
+                    <br /> <br />
+
+                    My expertise in the following technologies: <br />
+
+                    Front-end: React, React Router, CSS Frameworks (Tailwind, Bootstrap and React Bootstrap), DaisyUI <br />
+
+                    Back-end: Express JS <br />
+
+                    Tools: Vercel, Netlify, Firebase, Surge, npm, VS Code, Git, GitHub
+
+                </p>
+            </div>
         </div>
     );
 };
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Checkout = () => {
+    const [toast, setToast] = useState(false);
+
+
+    const handleConfirm = () => {
+
+        setToast(true);
+
+    }
+
     return (
-        <div>
-            <h2>This is Checkout</h2>
+        <div className='container mx-auto w-96 mt-16'>
+            <form>
+                <div>
+                    <label>Name</label>
+                    <br />
+                    <input className='mt-4' type="text" name="name" id="name" placeholder='Your Name' required />
+                </div>
+
+                <br />
+
+                <div>
+                    <label>Email Address</label>
+                    <br />
+                    <input className='mt-4' type="email" name="email" id="email" placeholder='Your Email' required />
+                </div>
+
+                <br />
+
+                <div>
+                    <label>Address</label>
+                    <br />
+                    <input className='mt-4' type="text" name="address" id="address" placeholder='Your Address' required />
+                </div>
+
+                <br />
+
+                <div>
+                    <label>Phone Number</label>
+                    <br />
+                    <input className='mt-4' type="text" name="phone" id="phone" placeholder='Phone Number' required />
+                </div>
+
+                <br />
+
+                <button onClick={handleConfirm} className="btn btn-primary">Confirm</button>
+
+                {
+                    toast && <div className="toast toast-top toast-end">
+                        <div className="alert alert-info">
+                            <span>Thank you for the booking</span>
+                        </div>
+                    </div>
+                }
+            </form>
         </div>
     );
 };
